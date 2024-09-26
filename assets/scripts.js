c0c6171f1e3870596c481b21ed27d07dc8e6a695
khoka-project <kokhaproject@gmail.com>
2024-09-26 22:48:11 +0200

    Add files via upload

---
+++ assets/scripts.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    var button = document.querySelector('.boton-scroll');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+};
